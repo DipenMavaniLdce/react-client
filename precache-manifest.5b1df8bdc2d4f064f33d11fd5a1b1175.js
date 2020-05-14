4db08d241e77acfad9d9104fcd9094f27e8c25f0
DipenMavaniLdce <62199316+DipenMavaniLdce@users.noreply.github.com>
2020-05-14 11:34:58 +0530

    Updates

---
+++ precache-manifest.5b1df8bdc2d4f064f33d11fd5a1b1175.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47d55b931b8babd3fa783b860c95052a",
+    "url": "/react-client/index.html"
+  },
+  {
+    "revision": "11eafadd2aa9eb3b7dd8",
+    "url": "/react-client/static/css/main.2561ec71.chunk.css"
+  },
+  {
+    "revision": "654da9c24918682bce36",
+    "url": "/react-client/static/js/2.cbe7d5cb.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/react-client/static/js/2.cbe7d5cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11eafadd2aa9eb3b7dd8",
+    "url": "/react-client/static/js/main.c0e4e398.chunk.js"
+  },
+  {
+    "revision": "289b8bdb222ce08f4862",
+    "url": "/react-client/static/js/runtime-main.feb6aa2c.js"
+  },
+  {
+    "revision": "8a4053ac585059669df2949d165471f6",
+    "url": "/react-client/static/media/login.8a4053ac.png"
+  }
+]);
